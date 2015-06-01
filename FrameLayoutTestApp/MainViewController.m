698f4c719f195486b7987eeb618244f33e613769
Pawel Scibek <pawel.scibek@polidea.com>
2015-06-01 15:02:05 +0200

    Initil commit

--- FrameLayoutTestApp/MainViewController.m
+++ FrameLayoutTestApp/MainViewController.m
@@ -6,22 +6,14 @@
 //  Copyright (c) 2015 Polidea. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "MainViewController.h"
 
-@interface ViewController ()
 
-@end
-
-@implementation ViewController
+@implementation MainViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
