b2e1da07a91d194ae7b782ecad9da1ffc77436d3
Pawel Scibek <pawel.scibek@polidea.com>
2015-06-02 13:45:22 +0200

    Cleaning directory tree

---
+++ FrameLayoutTestApp/classes/AppDelegate.m
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.m
+//  FrameLayoutTestApp
+//
+//  Created by Pawel Scibek on 01/06/15.
+//  Copyright (c) 2015 Polidea. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "ListViewController.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+   
+    ListViewController *mainViewController = [[ListViewController alloc] initWithStyle:UITableViewStylePlain];
+
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:mainViewController];
+    navigationController.navigationBar.translucent = NO;
+    
+    CGRect frame = [[UIScreen mainScreen] bounds];
+    UIWindow *window = [[UIWindow alloc]initWithFrame:frame];
+    window.rootViewController = navigationController;
+    [window makeKeyAndVisible];
+    self.window = window;
+    
+    return YES;
+}
+
+@end
